a61d43e35c01ccf323f3d1848673de67958695be
Heakl <ahmed.heakl@ejust.edu.eg>
2024-04-13 04:18:23 +0200

    Added default output path

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--shuffle_metadata", action="store_true")
     parser.add_argument("-k", "--k_shot", action="store_true")
     # execution-related parameters
-    parser.add_argument("-o", "--output_file", nargs="+", type=str, required=True)
+    parser.add_argument("-o", "--output_file", nargs="+", type=str, default=["output.csv"])
     parser.add_argument("-p", "--parallel_threads", type=int, default=5)
     parser.add_argument("-t", "--timeout_gen", type=float, default=30.0)
     parser.add_argument("-u", "--timeout_exec", type=float, default=10.0)
@@ -38,8 +38,6 @@ if __name__ == "__main__":
             "hf",
             "-f",
             "prompts/prompt_sqlcoder2.md",
-            "-o",
-            "output.csv",
         ]
     )
 
